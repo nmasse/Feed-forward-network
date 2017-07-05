3432e70bf36f3107cce3e58ba100d1659a29b947
nmasse <nicolas.masse@gmail.com>
2017-07-04 21:07:00 -0500

    Cleaned up z-scoring. Added second days_since_prev_entry value.

--- generate_data.py
+++ generate_data.py
@@ -8,12 +8,9 @@ class Data:
 
     def __init__(self, data_filename):
 
-        #f = 'C:/Users/nicol_000/Projects/Corey Train Analysis/IRRIS_Raw_2014-2016/good code/data_odd.mat'
-        #d = tables.open_file(data_filename)
 
         d = h5py.File(data_filename)
-        [print(p) for p in d['results'].keys()]
-        print(d['results']['vars'])
+
         self.input_data = np.array(d['results']['vars'])
         self.days_since_prev_entry = np.array(d['results']['days_since_prev_entry'])
         self.future_max_vert = np.array(d['results']['future_max_vert'])
@@ -29,10 +26,15 @@ class Data:
         self.num_vars = self.input_data.shape[0]
 
         # remove nans and z-score data
-        self.input_data[np.isnan(self.input_data)] = 0
-        u = np.tile(np.mean(self.input_data, axis=1, keepdims=True), self.input_data.shape[1])
-        sd = np.tile(np.std(self.input_data, axis=1, keepdims=True), self.input_data.shape[1])
+        u = np.tile(np.nanmean(self.input_data, axis=1, keepdims=True), (1,self.input_data.shape[1]))
+        sd = np.tile(np.nanstd(self.input_data, axis=1, keepdims=True), (1,self.input_data.shape[1]))
+        for d in range(self.num_vars):
+            ind = np.where(np.isnan(self.input_data[d,:]))[0]
+            self.input_data[d,ind] = u[d,0]
         self.input_data = (self.input_data-u)/sd
+
+        # cap at +/-6 SDs
+        self.input_data = np.maximum(-6, np.minimum(6,self.input_data))
 
         print('Number vars = ', self.num_vars)
 
@@ -46,13 +48,13 @@ class Data:
         else:
             num_reps = 1
 
-        x = np.zeros((num_reps, par['batch_size'], (self.num_vars+1)*par['hist_size']-1), dtype = np.float32)
-        y = np.zeros((par['batch_size'], 2), dtype = np.float32)
+        x = np.zeros((num_reps, par['batch_size'], (self.num_vars+2)*par['hist_size']-2), dtype = np.float32)
+        y = np.zeros((par['batch_size'], par['layer_dims'][-1]), dtype = np.float32)
 
         for i in range(par['batch_size']):
             # randomly select batch_size entries
             ind = self.valid_last_data_pts[np.random.randint(self.num_valid_last_data_pts)]
-            y[i,:] = self.future_max_vert[:,ind]
+            y[i,0] = self.future_max_vert[1,ind]
             ind_range = range(self.wheel_index_range[0,ind], self.wheel_index_range[1,ind])
             ind_preceeding = self.wheel_index_range[0,ind] + np.where(self.ID_count[0,ind_range] < self.ID_count[0,ind])[0]
 
@@ -70,9 +72,13 @@ class Data:
                     x[r,i,u] = self.input_data[:, ind_hist[j]]
 
                     # days since prev entry
-                    u = self.num_vars*par['hist_size']+j
-                    x[r,i,u] = self.days_since_prev_entry[0,ind_hist[j]]
+                    u1 = self.num_vars*par['hist_size']+j
+                    u2 = self.num_vars*par['hist_size']+par['hist_size']-1+j
+                    #  TESTING!
+                    x[r,i,u1] = 2*np.exp(-self.days_since_prev_entry[0,ind_hist[j]]/7)
+                    x[r,i,u2] = 2*np.exp(-self.days_since_prev_entry[0,ind_hist[j]]/30)
 
-        #plt.imshow(x, aspect='auto', interpolation = 'none')
+        #plt.imshow(np.squeeze(x), aspect='auto', interpolation = 'none')
+        #plt.colorbar()
         #plt.show()
         return np.squeeze(x), y
