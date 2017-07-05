ee4432ce65cc828a648916e837b288968a3e50c8
nmasse <nicolas.masse@gmail.com>
2017-07-04 21:08:23 -0500

    Evaluating model with multiple calls to generate_data.

--- model.py
+++ model.py
@@ -37,17 +37,17 @@ class Model:
         for n in range(par['num_layers']-1):
             scope_name = 'layer' + str(n)
             with tf.variable_scope(scope_name):
-                W = tf.get_variable('W', (par['layer_dims'][n+1], par['layer_dims'][n]), initializer=tf.random_normal_initializer())
+                W = tf.get_variable('W', (par['layer_dims'][n+1], par['layer_dims'][n]), initializer=tf.random_normal_initializer(0, par['init_weight_sd']))
                 b = tf.get_variable('b', (par['layer_dims'][n+1], 1), initializer=tf.constant_initializer(0))
                 if n == par['num_layers']-2:
                     # use linear activation function for last layer
-                    self.y_hat = tf.matmul(W, self.x_drop) + b
+                    self.y_hat = tf.matmul(W, self.x) + b
                 else:
                     # use sigmoid activation function for all other layers
-                    self.x = tf.sigmoid(tf.matmul(W, self.x) + b)
+                    self.x = tf.nn.relu(tf.matmul(W, self.x) + b)
                 if n == par['num_layers']-3:
                     # apply dropout right before final layer
-                    self.x_drop = tf.nn.dropout(self.x, self.keep_prob)
+                    self.x = tf.nn.dropout(self.x, self.keep_prob)
 
 
     def optimize(self):
@@ -89,23 +89,38 @@ def main():
 
             # generate batch of N (batch_size X num_batches) trials
             input_data, target_data = train_data.generate_batch_data(test_data = False)
-            test_input_data, test_target_data = train_data.generate_batch_data(test_data = True)
-            test_output = np.zeros((par['test_reps'], par['layer_dims'][-1], par['batch_size']), dtype = np.float32)
 
             if par['learning_rate']>0:
+
+                """
+                Train the model
+                """
                 _, train_loss, model_output = sess.run([model.minimize, model.loss, model.y_hat], {x: input_data.T, y: target_data.T, keep_prob: par['keep_prob']})
+
+                """
+                Test model on cross-validated data every 'iters_between_eval' trials
+                """
                 if (i+1)%par['iters_between_eval']==0:
-                    for j in range(par['test_reps']):
-                        test_output[j,:,:] = sess.run(model.y_hat, {x: test_input_data[j,:,:].T, y: test_target_data.T, keep_prob: np.float32(1)})
-                    test_output = np.mean(test_output, axis=0)
-                    test_loss = np.mean((test_output-test_target_data.T)**2)
+                    test_loss = np.zeros((10))
+                    for r in range(10):
+                        test_input_data, test_target_data = train_data.generate_batch_data(test_data = True)
+                        test_output = np.zeros((par['test_reps'], par['layer_dims'][-1], par['batch_size']), dtype = np.float32)
+                        for j in range(par['test_reps']):
+                            test_output[j,:,:] = sess.run(model.y_hat, {x: test_input_data[j,:,:].T, y: test_target_data.T, keep_prob: np.float32(1)})
+                        test_output = np.mean(test_output, axis=0)
+                        test_loss[r] = np.mean((test_output-test_target_data.T)**2)
+                    test_performance = append_data(test_performance, np.mean(test_loss), time, i, t_start)
             else:
                 loss, model_output = sess.run([model.loss, model.y_hat], {x: input_data, y: target_data[:,0]})
 
             train_performance = append_data(train_performance, train_loss, time, i, t_start)
 
+            # reduce learning rate if train loss below thresholds
+            if train_loss<60:
+                par['learning_rate'] = 2e-4
+
             if (i+1)%par['iters_between_eval']==0:
-                test_performance = append_data(test_performance, test_loss, time, i, t_start)
+
                 print_results(train_performance, test_performance)
 
 
