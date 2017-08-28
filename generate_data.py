c1c4b2ae6e0ecbcb313c1b30ca0f381118613884
gdgrant <gdgrant@uchicago.edu>
2017-08-28 12:52:52 -0500

    General revisions

--- generate_data.py
+++ generate_data.py
@@ -1,43 +1,45 @@
 import numpy as np
 from mnist import MNIST
 from parameters import *
-
+import itertools
 
 class Data:
 
     def __init__(self):
 
-        mndata = MNIST('C:/Users/nicol/Projects/GitHub/MNIST-dataset-in-different-formats/data/Original dataset/')
-        #mndata = MNIST('C:\Users\nicol\Projects\GitHub\MNIST-dataset-in-different-formats\data\Original dataset')
+        # Load MNIST data
+        mndata = MNIST('./resources/mnist/data/original')
         self.train_images, self.train_labels = mndata.load_training()
-        self.test_images, self.test_labels = mndata.load_testing()
+        self.test_images, self.test_labels   = mndata.load_testing()
 
-        print(len(self.train_images), len(self.test_images))
+        print('Num. available train images:', len(self.train_images))
+        print('Num. available test images: ', len(self.test_images))
 
-        # generate permutations
+        # Generate MNIST image permutations
         self.permutations = np.zeros((par['n_perms'], par['n_pixels']) ,dtype=np.int16)
-        for i in range(par['n_perms']):
+        for i in range(1, par['n_perms']):
             self.permutations[i, :] = np.random.permutation(par['n_pixels'])
 
 
-    def generate_batch_data(self, perm_ind = 0, test_data = False):
+    def generate_batch_data(self, perm_ind=0, test_data=False):
 
+        # Allocate x and y arrays
         x = np.zeros((par['n_pixels'], par['batch_size']), dtype = np.float32)
         y = np.zeros((10, par['batch_size']), dtype = np.float32)
 
+        # Generate appropriate indices for batch data
         if test_data:
             ind = np.random.permutation(len(self.test_labels))
         else:
             ind = np.random.permutation(len(self.train_labels))
         ind = ind[:par['batch_size']]
 
-        for i, img in enumerate(ind):
-            for j, p in enumerate(self.permutations[perm_ind,:]):
-                if test_data:
-                    x[j, i] = self.test_images[img][p]
-                    y[self.test_labels[img], i] = 1
-                else:
-                    x[j, i] = self.train_images[img][p]
-                    y[self.train_labels[img], i] = 1
+        for (i, img), (j, p) in itertools.product(enumerate(ind), enumerate(self.permutations[perm_ind,:])):
+            if test_data:
+                x[j,i] = self.test_images[img][p]/255
+                y[self.test_labels[img], i] = 1
+            else:
+                x[j,i] = self.train_images[img][p]/255
+                y[self.train_labels[img], i] = 1
 
         return x, y
--- model.py
+++ model.py
@@ -8,8 +8,12 @@ import numpy as np
 import generate_data
 import time
 from parameters import *
+import os
 
-# Reset TensorFlow before running anythin
+# Ignore startup TensorFlow warnings
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+# Reset TensorFlow before running anything
 tf.reset_default_graph()
 
 """
@@ -19,6 +23,8 @@ Model setup and execution
 class Model:
 
     def __init__(self, input_data, target_data, dendrite_clamp, keep_prob):
+
+        print('\nBuilding graph...')
 
         # Load the input activity, the target data, and the training mask for this batch of trials
         self.input_data = input_data
@@ -32,49 +38,60 @@ class Model:
         # Train the model
         self.optimize()
 
+        print('\nGraph built successfully.\n')
+
+
     def run_model(self):
 
         self.x = self.input_data
         for n in range(par['n_hidden_layers']):
-            scope_name = 'layer' + str(n)
-            with tf.variable_scope(scope_name):
+            with tf.variable_scope('layer' + str(n)):
+                print('\n-- Layer', n, '--')
+
+                # Get layer variables
                 W = tf.get_variable('W', (par['layer_dims'][n+1], par['layer_dims'][n], par['n_dendrites']), \
                     initializer=tf.random_normal_initializer(0, par['init_weight_sd']))
                 b = tf.get_variable('b', (par['layer_dims'][n+1], 1), initializer=tf.constant_initializer(0))
 
-                print('Layer ', n)
-                print(self.x)
-                print(W)
+                # Run layer calculations
                 x0 = tf.tensordot(W, self.x, ([1],[0]))
-                print(x0)
                 #x1 = tf.nn.relu(x0 - self.dendrite_clamp)
                 x1 = tf.nn.relu(x0)
-                print(x1)
+
+                # Print layer variables and run final layer calculation
                 self.x = tf.reduce_sum(x1,axis = 1) + b
-                print(self.x)
+                tf_var_print(self.x, W, x0, x1)
+
+                # Apply dropout right before final layer
                 if n == par['n_hidden_layers']:
-                    # apply dropout right before final layer
                     self.x = tf.nn.dropout(self.x, self.keep_prob)
 
+
         with tf.variable_scope('output'):
+            print('\n-- Output --')
+
+            # Get layer variables
             W = tf.get_variable('W', (par['layer_dims'][par['n_hidden_layers']+1], par['layer_dims'][par['n_hidden_layers']]), \
                 initializer=tf.random_normal_initializer(0, par['init_weight_sd']))
             b = tf.get_variable('b', (par['layer_dims'][par['n_hidden_layers']+1], 1), initializer=tf.constant_initializer(0))
 
+
+            # Run layer calculation
             self.y = tf.matmul(W, self.x) + b
+            tf_var_print(W, b, self.y)
 
 
     def optimize(self):
 
+        # Calculate loss and run optimization
         self.loss = tf.reduce_mean(tf.square(self.target_data - self.y))
         opt = tf.train.AdamOptimizer(learning_rate = par['learning_rate'])
         self.minimize = opt.minimize(self.loss)
 
+
 def main():
 
-    """
-    Create the stimulus class to generate trial paramaters and input activity
-    """
+    # Create the stimulus class, and generate trial paramaters and input activity
     stim = generate_data.Data()
 
     x = tf.placeholder(tf.float32, shape=[par['layer_dims'][0], par['batch_size']])  # input data
@@ -82,68 +99,71 @@ def main():
     dendrite_clamp = []
     keep_prob = tf.placeholder(tf.float32) # used for dropout
 
+    # Open TensorFlow session
     with tf.Session() as sess:
 
+        # Generate graph
         model = Model(x, y, dendrite_clamp, keep_prob)
         init = tf.global_variables_initializer()
         sess.run(init)
         t_start = time.time()
-        print('Graph created...')
 
-        saver = tf.train.Saver()
         # Restore variables from previous model if desired
+        saver = tf.train.Saver()
         if par['load_previous_model']:
             saver.restore(sess, par['save_dir'] + par['ckpt_load_fn'])
             print('Model ' +  par['ckpt_load_fn'] + ' restored.')
 
-        # keep track of the model performance across training
+        # Keep track of the model performance across training
         train_performance = {'loss': [], 'trial': [], 'time': []}
         test_performance = {'loss': [], 'trial': [], 'time': []}
 
         for i in range(par['num_iterations']):
 
-            # generate batch of N (batch_size X num_batches) trials
-            input_data, target_data = stim.generate_batch_data(perm_ind = 0, test_data = False)
+            # Generate batch of N (batch_size X num_batches) trials
+            perm_ind = (i//250)%par['n_perms']
+            input_data, target_data = stim.generate_batch_data(perm_ind=perm_ind, test_data=False)
 
-            if par['learning_rate']>0:
+            # Train the model
+            _, train_loss, model_output = sess.run([model.minimize, model.loss, model.y], \
+                {x: input_data, y: target_data, keep_prob: par['keep_prob']})
 
-                """
-                Train the model
-                """
-                _, train_loss, model_output = sess.run([model.minimize, model.loss, model.y], \
-                    {x: input_data, y: target_data, keep_prob: par['keep_prob']})
-
-                """
-                Test model on cross-validated data every 'iters_between_eval' trials
-                """
-                if (i+1)%par['iters_between_eval']==0:
-                    test_loss = np.zeros((10))
-                    for r in range(10):
-                        test_input_data, test_target_data = stim.generate_batch_data(test_data = True)
-                        test_output = np.zeros((par['test_reps'], par['layer_dims'][-1], par['batch_size']), dtype = np.float32)
-                        for j in range(par['test_reps']):
-                            test_output[j,:,:] = sess.run(model.y_hat, {x: test_input_data[j,:,:].T, y: test_target_data.T, keep_prob: np.float32(1)})
-                        test_output = np.mean(test_output, axis=0)
-                        test_loss[r] = np.mean((test_output-test_target_data.T)**2)
-                    test_performance = append_data(test_performance, np.mean(test_loss), time, i, t_start)
-            else:
-                loss, model_output = sess.run([model.loss, model.y_hat], {x: input_data, y: target_data[:,0]})
-
+            # Append performance data
             train_performance = append_data(train_performance, train_loss, time, i, t_start)
 
-            # reduce learning rate if train loss below thresholds
+            # Test model on cross-validated data every 'iters_between_eval' trials
+            if (i+1)%par['iters_between_eval']==0:
+                test_loss = np.zeros((10))
+                for r in range(10):
+                    # Generate batch of trials
+                    test_input_data, test_target_data = stim.generate_batch_data(test_data=True)
+                    test_output = np.zeros((par['test_reps'], par['layer_dims'][-1], par['batch_size']), dtype=np.float32)
+
+                    # Test the model
+                    for j in range(par['test_reps']):
+                        test_output[j,:,:] = sess.run(model.y, {x: test_input_data[:,:], y: test_target_data, keep_prob: np.float32(1)})
+
+                    # Average across test reps. and calculate MSE loss
+                    test_output = np.mean(test_output, axis=0)
+                    test_loss[r] = np.mean((test_output-test_target_data)**2)
+
+                # Append performance data
+                test_performance = append_data(test_performance, np.mean(test_loss), time, i, t_start)
+
+            # Reduce learning rate if train loss below thresholds
             if train_loss<60:
                 par['learning_rate'] = 2e-4
 
-            if (i+1)%par['iters_between_eval']==0:
+            # Print results and associated data
+            if i%par['iters_between_eval']==0 and i != 0:
+                print_results(train_performance, test_performance, perm_ind)
 
-                print_results(train_performance, test_performance)
 
-
-def print_results(train_performance, test_performance):
+def print_results(train_performance, test_performance, perm_ind):
 
     print('Trial {:7d}'.format(train_performance['trial'][-1]) +
-      ' | Time {:0.2f} s'.format(train_performance['time'][-1]) +
+      ' | Perm. {:2d}'.format(perm_ind) +
+      ' | Time {:6.2f} s'.format(train_performance['time'][-1]) +
       ' | Train loss {:0.4f}'.format(np.mean(train_performance['loss'][-par['iters_between_eval']:])) +
       ' | Test loss {:0.4f}'.format(test_performance['loss'][-1]))
 
@@ -155,5 +175,11 @@ def append_data(d, loss, time, i, t_start):
 
     return d
 
+def tf_var_print(*var):
+    for v in var:
+        print(str(v.name).ljust(20), v.shape)
 
-main()
+try:
+    main()
+except KeyboardInterrupt:
+    print('Quit by KeyboardInterrupt.')
