3504baca4d90a25ac32654ab365509d515bef186
gdgrant <gdgrant@uchicago.edu>
2017-08-30 13:52:40 -0500

    Fixed zero grad bug - had failed to select zeroth permutation

--- generate_data.py
+++ generate_data.py
@@ -17,7 +17,7 @@ class Data:
 
         # Generate MNIST image permutations
         self.permutations = np.zeros((par['n_perms'], par['n_pixels']) ,dtype=np.int16)
-        for i in range(1, par['n_perms']):
+        for i in range(par['n_perms']):
             self.permutations[i, :] = np.random.permutation(par['n_pixels'])
 
 
@@ -36,10 +36,10 @@ class Data:
 
         for (i, img), (j, p) in itertools.product(enumerate(ind), enumerate(self.permutations[perm_ind,:])):
             if test_data:
-                x[j,i] = self.test_images[img][p]/255
+                x[j,i] = np.float32(self.test_images[img][p])/255
                 y[self.test_labels[img], i] = 1
             else:
-                x[j,i] = self.train_images[img][p]/255
+                x[j,i] = np.float32(self.train_images[img][p])/255
                 y[self.train_labels[img], i] = 1
 
         return x, y
